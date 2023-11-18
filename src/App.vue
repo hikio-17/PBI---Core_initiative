43bd0bcddf02b2046736e183d8ba064d7b1ba2f1
hikio-17 <fajri010217@gmail.com>
2023-11-18 14:04:36 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="bg-gray-100">
+    <NotificationPage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import NotificationPage from './components/NotificationPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NotificationPage
+  }
+}
+</script>
+
